34e74a1c03b8d3a5d615e9ad60333eb029d99470
Walter Brien <44796480+DeltaX75@users.noreply.github.com>
2019-12-17 22:49:21 +0800

    Update 冒泡排序.cpp

--- Algorithm Series/排序算法/冒泡排序/冒泡排序.cpp
+++ Algorithm Series/排序算法/冒泡排序/冒泡排序.cpp
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #define N 10
 void Swap(int *a,int *b);
+void Sort(int x[]);
 int main()
 {
 	int a[N]={4,8,-8,3,5,7,-5,11,6,15};
 	int i,j;
-
+/*	
 	for(i=0;i<N;++i)
 		for(j=0;j<N-i;++j)
 			if(a[j] > a[j+1])
 				Swap(&a[j],&a[j+1]);
-
+*/
+	Sort(a);
 	for(i=0;i<N;++i)
-		printf("%d \n",a[i]);
-
+	printf("%d \n",a[i]);
 	return 0;
 }
 
@@ -23,4 +24,13 @@ void Swap(int *a,int *b)
 	t=*a;
 	*a=*b;
 	*b=t;
+}
+
+void Sort(int x[])
+{
+	int i,j;
+	for(i=0;i<N;++i)
+		for(j=0;j<N-i;++j)
+			if(x[j] > x[j+1])
+				Swap(&x[j],&x[j+1]);
 }
