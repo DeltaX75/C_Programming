f2ae7df738cada76ac1f8919549fc924c721b512
Walter Brien <44796480+DeltaX75@users.noreply.github.com>
2019-12-17 22:43:43 +0800

    Update 冒泡排序.cpp

--- Algorithm Series/排序算法/冒泡排序/冒泡排序.cpp
+++ Algorithm Series/排序算法/冒泡排序/冒泡排序.cpp
@@ -3,7 +3,17 @@
 void Swap(int *a,int *b);
 int main()
 {
-	int a[N];
+	int a[N]={4,8,-8,3,5,7,-5,11,6,15};
+
+	int i,j;
+
+	for(i=0;i<N;++i)
+		for(j=0;j<N-i;++j)
+			if(a[j] > a[j+1])
+				Swap(&a[j],&a[j+1]);
+
+	for(i=0;i<N;++i)
+		printf("%d \n",a[i]);
 
 	return 0;
 }
@@ -13,5 +23,5 @@ void Swap(int *a,int *b)
 	int t;
 	t=*a;
 	*a=*b;
-	*b=*t;
+	*b=t;
 }
